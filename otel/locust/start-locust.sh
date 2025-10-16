8a9bc9694b14137b0fe5e98cbb06758cc748ec55
Junwei Chan <42599677+mrjunweichan@users.noreply.github.com>
2025-10-16 12:40:05 +0800

    Update start-locust.sh

--- otel/locust/start-locust.sh
+++ otel/locust/start-locust.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 locust -f /mnt/locust/locustfile.py &
 LOCUST_PID=$!
-sleep 5
-python3 -c "import requests; requests.post('http://localhost:8089/swarm', data={'user_count': 15, 'spawn_rate': 10})"
+sleep 10
+for i in {1..5}; do
+  python3 -c "import requests; requests.post('http://localhost:8089/swarm', data={'user_count': 15, 'spawn_rate': 10})" && break
+  echo "Attempt $i: Failed to start swarm, retrying in 5 seconds..."
+  sleep 5
+done
 wait $LOCUST_PID
